d19bb05a2fea59899cc3098029d57947a56b8882
shun-h2 <46297054+shun-h2@users.noreply.github.com>
2020-02-21 21:28:10 +0800

    Update reader.py
    Each function takes in 2 arguments:
    Location of the file to read
    Feature file to use as header for final output

--- reader.py
+++ reader.py
@@ -1,15 +1,13 @@
 import pandas as pd
 
 
-def read_csv():
-    # Keeps prompting user to enter file location until a valid file location is entered
+def read_csv(file, feature):
     while True:
         try:
-            file = input("File location: ")
             # Display maximum number of columns
             pd.set_option("display.width", None)
             # Index 1 / Column 2 of features csv is stored as category
-            category = pd.read_csv("NUSW-NB15_features.csv", encoding="ISO-8859-1", index_col=1)
+            category = pd.read_csv(feature, encoding="ISO-8859-1", index_col=1)
             # Reads data from file and stores in dataSet variable with category as column names
             dataSet = pd.read_csv(file, encoding="ISO-8859-1", names=category.index, dtype="unicode").fillna(0)
             return dataSet, category.index, True
@@ -19,15 +17,13 @@ def read_csv():
             return False
 
 
-def read_xlsx():
-    # Keeps prompting user to enter file location until a valid file location is entered
+def read_xlsx(file, feature):
     while True:
         try:
-            file = input("File location: ")
             # Display maximum number of columns
             pd.set_option("display.width", None)
             # Index 1 / Column 2 of features csv is stored as category
-            category = pd.read_csv("NUSW-NB15_features.csv", encoding="ISO-8859-1", index_col=1)
+            category = pd.read_csv(feature, encoding="ISO-8859-1", index_col=1)
             # Reads data from file and stores in dataSet variable with category as column names
             dataSet = pd.read_excel(file, names=category.index).fillna(0)
             return dataSet, category.index, True
