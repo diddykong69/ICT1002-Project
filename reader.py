0fc3fc30a2d8692a41fa817e35b24cc77571fb6f
shun-h2 <46297054+shun-h2@users.noreply.github.com>
2020-02-22 00:21:08 +0800

    Update reader.py
    Added variable 'success' to hold True/False status when program runs.

--- reader.py
+++ reader.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def read_csv(file, feature):
+    success = True
     while True:
         try:
             # Display maximum number of columns
@@ -10,14 +11,17 @@ def read_csv(file, feature):
             category = pd.read_csv(feature, encoding="ISO-8859-1", index_col=1)
             # Reads data from file and stores in dataSet variable with category as column names
             dataSet = pd.read_csv(file, encoding="ISO-8859-1", names=category.index, dtype="unicode").fillna(0)
-            return dataSet, category.index, True
+            return dataSet, category.index, success
         except FileNotFoundError:
-            return False
+            success = False
+            return success
         except pd.errors.ParserError:
-            return False
+            success = False
+            return success
 
 
 def read_xlsx(file, feature):
+    success = True
     while True:
         try:
             # Display maximum number of columns
@@ -26,11 +30,13 @@ def read_xlsx(file, feature):
             category = pd.read_csv(feature, encoding="ISO-8859-1", index_col=1)
             # Reads data from file and stores in dataSet variable with category as column names
             dataSet = pd.read_excel(file, names=category.index).fillna(0)
-            return dataSet, category.index, True
+            return dataSet, category.index, success
         except FileNotFoundError:
-            return False
+            success = False
+            return success
         except pd.errors.ParserError:
-            return False
+            success = False
+            return success
 
 
 # Calls function if file is called as a script
