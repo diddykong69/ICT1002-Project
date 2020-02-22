c3f3cff999aa5863ab8a55eff0209a7b9863780a
shun-h2 <46297054+shun-h2@users.noreply.github.com>
2020-02-22 17:23:50 +0800

    Update reader.py
    - Added to_text and show_output function
    - Remove headers and add headers as first row instead (WIP)

--- reader.py
+++ reader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import csv
 
 def read_csv(file, feature):
     success = True
@@ -11,6 +11,8 @@ def read_csv(file, feature):
             category = pd.read_csv(feature, encoding="ISO-8859-1", index_col=1)
             # Reads data from file and stores in dataSet variable with category as column names
             dataSet = pd.read_csv(file, encoding="ISO-8859-1", names=category.index, dtype="unicode").fillna(0)
+            # Change the name of the text file to the name that user input WIP
+            to_text(dataSet, "data.txt")
             return dataSet, category.index, success
         except FileNotFoundError:
             success = False
@@ -30,6 +32,8 @@ def read_xlsx(file, feature):
             category = pd.read_csv(feature, encoding="ISO-8859-1", index_col=1)
             # Reads data from file and stores in dataSet variable with category as column names
             dataSet = pd.read_excel(file, names=category.index).fillna(0)
+            # Change the name of the text file to the name that user input WIP
+            to_text(dataSet, "data.txt")
             return dataSet, category.index, success
         except FileNotFoundError:
             success = False
@@ -37,9 +41,28 @@ def read_xlsx(file, feature):
         except pd.errors.ParserError:
             success = False
             return success
+  
+# Outputs data of file read into a text file for database purposes
+def to_text(data, location):
+    csv_file = data.to_csv("data.csv", index=False)
+    with open(location, "w") as output_file:
+        with open("data.csv", "r") as input_file:
+            [output_file.write("\t".join(row)+'\n') for row in csv.reader(input_file)]
+        output_file.close()
+        
+ 
+# When function is called, prints output to the user in a user friendly manner
+def show_output(data):
+    lines = len(data.index)
+    i = 0
+    while i < lines:
+        print(data.iloc[i:i+5])
+        input("Press enter to read more...")
+        i += 5
 
 
 # Calls function if file is called as a script
 if __name__ == "__main__":
     read_csv()
     read_xlsx()
+    show_output()
