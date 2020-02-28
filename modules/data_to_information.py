26c3b629057fc895b6b7d852e393e3fbc7edb684
raymousray <61280887+raymousray@users.noreply.github.com>
2020-02-28 13:54:14 +0800

    Update data_to_information.py

--- modules/data_to_information.py
+++ modules/data_to_information.py
@@ -1,10 +1,8 @@
 import sys
-# import csv
 import pandas as pd
-# import matplotlib.pyplot as plt
-# from vincent.colors import brews
 from collections import Counter
 
+# bring data and put into dataframe. Input require reader: dataset, columns: variable
 def dataframe(reader, name):
     list2 = []
     dict3 = {}
@@ -20,20 +18,25 @@ def dataframe(reader, name):
     df2 = pd.DataFrame([dict3], index=['count'])
     return df2
 
-
+# bring data and put into dataframe for line graph only. Input require reader: dataset, will only select Stime for the timeline
 def timelinedict(reader2):
 
     df = pd.DataFrame(data=reader2, columns=['Stime'])
     list1 = df.values.tolist()
     list2 = []
     dict2 = {}
+    # ensure the data are in string format
     for i in list1:
         for a in i:
             list2 += [str(a)]
     dict1 = (Counter(list2))
+    # from the dictionary dict1, convert all the keys into unix time(hours only) and place into the list hours
     hours = [(pd.to_datetime(time, unit='s').hour, time) for time in dict1.keys()]
+    # create another list hours_2 with the keys 0-23 to represent the hours of the day with the value 0
     hours_2 = {hour: 0 for hour in range(24)}
+    
     dict1_keys = [key for key in dict1.keys()]
+    # comparing the keys from hours and dict1, if same append to hours_2
     for i in range(len(dict1.items())):
         if hours[i][1] == dict1_keys[i]:
             hours_2[hours[i][0]] += int(dict1.get(dict1_keys[i]))
@@ -94,7 +97,6 @@ def createchart(type, dataframe, name, writer):
     # Insert the chart into the worksheet.
     worksheet.insert_chart('B4', chart)
     # Close the Pandas Excel writer and output the Excel file.
-    # print('here')
 
 
 # Create a Pandas Excel writer using XlsxWriter as the engine.
@@ -102,7 +104,7 @@ def createchart(type, dataframe, name, writer):
 def data_to_information(file, location):
     try:
         excel_file = location
-        # file = pd.read_csv(file)
+        # execute all the function to write out the chart
         writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
         createchart('pie', dataframe(file, 'srcip'), 'source host', writer)
         createchart('column', dataframe(file, 'sport'), 'source port', writer)
@@ -113,8 +115,6 @@ def data_to_information(file, location):
         writer.close()
         return True
     except:
-        # e = sys.exc_info()[0]
-        # print("Error: %s" % e.with_traceback())
-        # print(e)
+        #in case of error
         return False
 
