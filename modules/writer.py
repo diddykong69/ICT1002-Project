ab23f119d56db37d14a538f8951fd616058a15bd
shun-h2 <how.shunhan@gmail.com>
2020-02-28 18:03:17 +0800

    Update writer.py

--- modules/writer.py
+++ modules/writer.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import pathlib
 
 
 def write_csv(dataSet, location):
     success = True
+    if isinstance(location, pathlib.Path):
+        location = location.name
+    else:
+        location = location
     try:
         dataSet.to_csv(location + ".csv", index=False)
         return success
